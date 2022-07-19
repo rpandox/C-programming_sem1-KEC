6b24abb3899ee91c907eab2124c783aa14c28a9a
rpandox <88703488+rpandox@users.noreply.github.com>
2022-07-19 21:40:30 +0545

    lab 2.1  done

--- lab_two/one.c
+++ lab_two/one.c
@@ -2,3 +2,55 @@
 a program that reads a co-ordinate point of rectangle coordinate system and displays the
 the appropriate message after checking where the point lies on the co-ordinate system.
 */
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    printf("enter x,y : ");
+    scanf("%d,%d",&x,&y);
+    if (x == 0)
+    {
+        if (y == 0)
+        {
+            printf("the point is origin");
+        }
+        else if(y > 0 || y < 0)
+        {
+            if (y > 0)
+                printf("the point (%d,%d) lies on y axis above x axis",x,y);
+            else 
+                printf("the point (%d,%d) lies on y axis below x axis",x,y);
+        }
+    }
+    else if (x > 0)
+    {
+        if (y == 0)
+        {
+            printf("the point (%d,%d) lies on x axis right of y axis",x,y);
+        }
+        else if(y > 0 || y < 0)
+        {
+            if (y > 0)
+                printf("the point (%d,%d) lies on First Quadrent",x,y);
+            else 
+                printf("the point (%d,%d) lies on Fourth Quadrent",x,y);
+        }
+    }
+    else 
+    {
+        if (y == 0)
+        {
+            printf("the point (%d,%d) lies on x axis left of y axis",x,y);
+        }
+        else if(y > 0 || y < 0)
+        {
+            if (y > 0)
+                printf("the point (%d,%d) lies on Second Quadrent",x,y);
+            else 
+                printf("the point (%d,%d) lies on Third Quadrent",x,y);
+        }
+    }
+    return 0;
+
+}
