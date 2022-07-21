146e0ddba17d8d12391c2b91f41fd8c710f8e859
rpandox <88703488+rpandox@users.noreply.github.com>
2022-07-21 22:52:17 +0545

    lab 2.2 done

--- lab_two/two.c
+++ lab_two/two.c
@@ -5,3 +5,26 @@ a program to find the taxable amount of an employee.
     b. 15% tax for 1,00,000 to 1,75,000 per annual income
     c. 25% tax for greater than 1,75,000 per annual income
 */
+#include <stdio.h>
+
+int main(void)
+{
+    double Sal, tax = 0;
+    printf("enter your Salary : ");
+    scanf("%lf",&Sal);
+
+    if (Sal <= 100000)
+        printf("there is no tax for you\n");
+    else
+    {
+        if (Sal > 100000 && Sal <= 175000)
+            tax = 0.15 * (Sal -100000) ;
+
+        if (Sal > 175000)
+            tax = 0.15 * 75000 + 0.25 * (Sal - 175000);      
+    }
+    printf("the total Tax for your Salary of %.2lf is %.2lf \n",Sal , tax);
+
+    return 0;
+
+}
