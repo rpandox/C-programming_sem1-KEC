8d91c43534f196992a561d185fc6dbc6bac129ba
rpandox <88703488+rpandox@users.noreply.github.com>
2022-07-25 20:41:12 +0545

    lab 3.1 done

--- lab_three/one.c
+++ lab_three/one.c
@@ -12,4 +12,54 @@ entered as inputs. Using switch case
                 Quantity(nos.): 3
                 The total cost of your choice will be Rs. 90.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int choice, quantity, price;
+    int price_1, price_2, price_3;
+    printf("\t\t\t\tDEPARTMENTAL STORE \n");
+    printf("\tS.N.    \t\tItem    \t\tPrice(Rs.)\n");
+    printf("\t1       \t\tBread   \t\t30\n");
+    printf("\t2       \t\tJam     \t\t90\n");
+    printf("\t3       \t\tButter  \t\t120\n");
+
+
+    price_1 = 30;
+    price_2 = 90;
+    price_3 = 120;
+
+    one:                //one is here for line 57
+    printf("Please enter your choice(1,2 or 3): ");
+    scanf("%d",&choice);
+
+    printf("Quantity(nos.): ");
+    scanf("%d",&quantity);
+
+    switch (choice)
+    {
+    case 1:
+        price = price_1 *quantity;
+        break;
+
+    case 2:
+        price = price_2 *quantity;
+        break;
+
+    case 3:
+        price = price_3 *quantity;
+        break;
+    
+    default:
+        printf("ERROR IN CHOICE\n");
+        printf("please enter again\n");
+        goto one;
+        break;
+    }
+
+printf("The total cost of your choice will be Rs. %d\n",price);
+return 0;
+
+}
     
