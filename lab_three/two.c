648480bd9cb22036228c391aa75757a575934146
rpandox <88703488+rpandox@users.noreply.github.com>
2022-07-25 20:48:46 +0545

    lab 3.2 done

--- lab_three/two.c
+++ lab_three/two.c
@@ -1,3 +1,31 @@
 /*
 a program to test whether a given year is a leap year
 */
+#include <stdio.h>
+
+int main (void)
+    {
+    int year;
+
+    printf("Enter the year :");
+    scanf("%d",&year);
+
+    if (year % 100 == 0)
+    {
+        if (year % 400 == 0)
+        printf("the year %d is a leap year\n",year);
+        else
+        printf("the year %d is a not leap year\n",year);
+    }
+    else
+    {
+        if (year % 4 == 0)
+        printf("the year %d is a leap year\n",year);
+        else
+        printf("the year %d is a not leap year\n",year);
+
+    }
+
+    return 0;
+    
+}
