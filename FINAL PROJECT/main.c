8d090977ad9b0e056dac77ec28b87701e938f0a8
rpandox <88703488+rpandox@users.noreply.github.com>
2022-07-27 19:12:03 +0545

    entry page exit page to project

--- FINAL PROJECT/main.c
+++ FINAL PROJECT/main.c
@@ -15,16 +15,29 @@ This is for hotel management system
 // #include <conio.h> NO CONIO ON MAC
  
 
-typedef struct emp
+typedef struct emp_desk
 {
     char emp_name[50];      // name of employee
     int emp_id;             //employee id 
     char password[10];      //employee password
     int emp_sal;            //employee salary
     long long emp_phone;    //employee phone 
-    int bonus;              // bonus for employee bonus = (0.01 of salary) per guest
+    int emp_bonus;          // bonus for employee bonus = (0.01 of salary) per guest
     int guest_count;        // guest for the employee
-}emp;
+    int type;               // parttime or not
+}desk_emp;
+
+typedef struct emp_service
+{
+    char emp_name[50];      // name of employee
+    int emp_id;             //employee id 
+    char password[10];      //employee password
+    int emp_sal;            //employee salary
+    long long emp_phone;    //employee phone 
+    int emp_bonus;          // bonus for employee bonus = (0.01 of salary) per guest
+    int guest_count;        // guest for the employee
+    int type;               // parttime or not
+}ser_emp;
 
 typedef struct admin
 {
@@ -38,14 +51,18 @@ typedef struct gue
     char gue_name[50];
     long long gue_number;
     int gue_room_no;
-    int total price;
+    int total_price;
+    int times_visited;
+    int loyalty_points;
+    char room_service_needed;
+    
 
 }gue;
 
 
 
 
-//THIS IS THE ENTRY PAGE
+/*
 void Entry_page()
 {
     printf(" ________________________________________________________________________\n");
@@ -74,15 +91,67 @@ void Entry_page()
     printf("| _______________________________________________________________________|\n");
 
 }
+*/
+
+
+void page_header()
+{
+    time_t t;
+    t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    printf(" ________________________________________________________________________\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t %d-%d-%d \t |\n",tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
+    
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\tTHIS IS THE MANAGEMENT SYSTEM FOR A HOTEL XXX\t\t |\n");
+    printf("|\t   +=================================================+\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+}
+
+
+void body_entrypage()
+{
+    printf("|\t\t\t\tLOGIN PAGE\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("| \t\t\t1. ADMINISTRATOR\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("| \t\t\t2. HOTEL EMPLOYEE\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("| \t\t\t3. EXIT\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    
+}
+
+void page_bottom()
+{
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("| _______________________________________________________________________|\n");
+
+}
+
+
+
 
 // This is choice for entry page
 int choice_entry_page()
 {
     char c,choice;
 
-
-    Entry_page();           // prints login page
-    c = getchar();          // enter choice
+    // prints login page
+    page_header();
+    body_entrypage();
+    page_bottom();
+               
+    scanf("%c",&c);         // enter choice
     choice = atoi(&c);      // take address
     //system("cls");
     system("clear");        // clear screen
@@ -114,8 +183,54 @@ void guest()
 
 void terminate()
 {
+    time_t t;
+    t = time(NULL);
+    struct tm tm = *localtime(&t);
     char c;
-     printf(" ________________________________________________________________________\n");
+    printf(" ________________________________________________________________________\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t %d-%d-%d \t |\n",tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\tYOU HAVE EXITED THE PROGRAM\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\tPress Any Key\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("| _______________________________________________________________________|\n");
+    getchar();
+
+}
+
+void errorpage()
+{
+    time_t t;
+    t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf(" ________________________________________________________________________\n");
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+    printf("|\t\t\t\t\t\t\t %d-%d-%d \t |\n",tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+     printf("|\t\t\t\t\t\t\t\t\t |\n");
+     printf("|\t\t\t\tERROR\t\t\t\t\t |\n");
      printf("|\t\t\t\t\t\t\t\t\t |\n");
      printf("|\t\t\t\t\t\t\t\t\t |\n");
      printf("|\t\t\t\t\t\t\t\t\t |\n");
@@ -125,57 +240,26 @@ void terminate()
      printf("|\t\t\t\t\t\t\t\t\t |\n");
      printf("|\t\t\t\t\t\t\t\t\t |\n");
      printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\tYOU HAVE EXITED THE PROGRAM\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\tPress Any Key\t\t\t |\n");
+     printf("|\t\t\t\t\tENTER AGAIN ?(y/n)\t\t |\n");
      printf("|\t\t\t\t\t\t\t\t\t |\n");
      printf("|\t\t\t\t\t\t\t\t\t |\n");
      printf("| _______________________________________________________________________|\n");
-
+     getchar();
 }
 
 int error()
 {
-    char e;
-    printf(" ________________________________________________________________________\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\tERROR\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\tENTER AGAIN ?(y/n)\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("|\t\t\t\t\t\t\t\t\t |\n");
-     printf("| _______________________________________________________________________|\n");
-
-     e = toupper(getchar());  
-     if (e == 'Y')
-                return 1;
-            else
-            return 0;
+    char e,f;
+    int i;
+     scanf("%c", &e);
+    f = toupper(e);
+    if (e == 'Y')
+        i = 1;
+    else
+        i = 0;
+        return i;
 }
+
 
 
 
@@ -183,22 +267,23 @@ int error()
 
 int main (void)
 {
-    int choice;
+    
+
+
+    int ch;
 
     // for error
     char errror;
     int t;
-
     top:
     //system("cls");
     system("clear");
 
-    choice = choice_entry_page();
+    ch = choice_entry_page();
 
-    switch (choice)
+    switch (ch)
     {
-        case 0:
-        return 0;
+
         case 1:
             administrator();
             break;
@@ -207,22 +292,20 @@ int main (void)
             employee();
             break;
         case 3:
-            guest();
-            break;
-        case 4:
             exit:
             //system("cls");
             system("clear");
             terminate();
-            getchar();
             //system("cls");
             //system("clear");
             return 0;
             break;
         
         default:
+            system("clear");
+            errorpage();
             t = error();
-            if (t)
+            if (t == '1')
                 goto top;
             else
             {
